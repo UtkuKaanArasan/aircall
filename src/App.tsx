2acf4db14661d97a2c4b6bba623ea4aaa4d30648
UtkuKaanArasan <utkukaan000@protonmail.ch>
2022-07-03 01:29:27 +0300

    initial commit, succesfully got data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+const axios = require('axios').default;
 
 function App() {
+
+  const access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJDb3N5bGltZTAwMCIsInVzZXJuYW1lIjoiQ29zeWxpbWUwMDAiLCJpYXQiOjE2NTY4MDA4MzksImV4cCI6MTY1NjgwMTQzOX0.kxrsyyweb99xOipZO30RZjAx-nN7O5-tsdZDz1YxP9A';
+
+  const [calls, setCalls] = useState([])
+
+  console.log(calls);
+  
+
+  function getCalls() {
+    axios.get('https://frontend-test-api.aircall.io/calls?offset=5&limit=5', {
+      headers: {
+        "Authorization": `Bearer ${access_token}`
+      }
+    }).then((res: any) => setCalls(res.data.nodes))
+      .catch((err: any) => console.log(err))
+  }
+
+  useEffect(() => { getCalls() }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {calls.map(
+          (item: any) => {
+            return <li>{item.from}</li>
+          }
+        )}
+      </ul>
     </div>
   );
 }
