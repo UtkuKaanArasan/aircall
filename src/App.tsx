789976729a003095fa83be2634588cdf1b4d9fea
UtkuKaanArasan <utkukaan000@protonmail.ch>
2022-07-05 02:08:12 +0300

    Readme updated temporarily

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const axios = require('axios').default;
 
 function App() {
   // Access key changes every 10 minutes so assinged a variable to it
-  const access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJDb3N5bGltZTAwMCIsInVzZXJuYW1lIjoiQ29zeWxpbWUwMDAiLCJpYXQiOjE2NTY5NzM2NjYsImV4cCI6MTY1Njk3NDI2Nn0.XfXL43yw1ByKlRjnRnaIZpA0jJIVFOHuxvD1iD4Q7Xk';
+  const access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJDb3N5bGltZTAwMCIsInVzZXJuYW1lIjoiQ29zeWxpbWUwMDAiLCJpYXQiOjE2NTY5NzQzNTMsImV4cCI6MTY1Njk3NDk1M30.aaUgrrK_hAtzG8eqTO07I3B4dn2K-p_iZW352VSg72c';
 
   function getCalls(limit:number) {
     axios.get(`https://frontend-test-api.aircall.io/calls?offset=${limit}&limit=${limit}`, {
